faaf3556cf49ffb002aa63a8b1ad238aaada2f2f
Amirul-group <illegalhero3@gmail.com>
2026-08-27 08:07:02 -0700

    gdfhg

--- home.php
+++ home.php
@@ -22,7 +22,7 @@ if(isset($_GET['logout'])){
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>home</title>
+   <title>index</title>
 
     <!-- custom css file link  -->
     <link rel="stylesheet" href="css/style.css">
@@ -57,7 +57,7 @@ if(isset($_GET['logout'])){
       <h3><?php echo $fetch['nid_number']; ?></h3>
 <!--
       <a href="update_profile.php" class="btn">update profile</a>
-      <a href="home.php?logout=<?php echo $user_id; ?>" class="delete-btn">logout</a>
+      <a href="index.php?logout=<?php echo $user_id; ?>" class="delete-btn">logout</a>
       <p>new <a href="login.php">login</a> or <a href="register.php">register</a></p>
 -->
    </div>
