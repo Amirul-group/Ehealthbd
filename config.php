d692a8d176f3628aa19b3164950a7f0827310001
Amirul-group <illegalhero3@gmail.com>
2026-08-28 10:22:13 -0700

    gjhg

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 
-$conn = mysqli_connect('sql113.infinityfree.com','if0_42773547','0zLQZhzeEsNDV','if0_42773547_web') or die('connection failed');
+$conn = mysqli_connect('sql113.infinityfree.com','if0_42773547_web','if0_42773547','0zLQZhzeEsNDV') or die('connection failed');
 
 ?>
